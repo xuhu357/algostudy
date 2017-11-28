171bde931495331022fffe1f0f8f2515aa336f26
xuhu357 <xuhu357@gmail.com>
2017-11-28 23:08:02 +0900

    조합 나열하기 문제 풀기. 4개 원소중 2개를 고르는 모든 방법을 나열하기.

---
+++ AlgoProblemSolvingBook/src/edu/happpylearn/subset/choose/ChooseMain.java
@@ -0,0 +1,46 @@
+package edu.happpylearn.subset.choose;
+
+import java.util.Scanner;
+
+public class ChooseMain {
+	int set[];
+	
+	public static void main(String[] args) {
+		ChooseMain m = new ChooseMain();
+		m.solve();
+	}
+
+	private void solve() {
+		// Get the input from standard input.
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int m = scan.nextInt();
+		scan.close();
+		
+		set = new int[n];
+		
+		chooseAndPrintNum(set, 0, n, m, 0); // c(n,m) = c(n-1,m-1) + c(n-1, m)
+		
+	}
+	
+	public void chooseAndPrintNum(int set[], int setSize, int n, int m, int index){
+		if(m == 0){ 			// no need to choose any more.
+			printData(set, setSize);
+		}else if(index == n){	// already hover all the element
+			return;
+		}else{					// set still not full
+			set[setSize] = index;
+			chooseAndPrintNum(set, setSize+1, n, m-1, index+1); // when element selected.
+			chooseAndPrintNum(set, setSize, n, m, index+1);	// when element not selected.
+		}
+		
+	}
+
+	private void printData(int[] set, int setSize) {
+		for(int i=0; i<setSize; i++){
+			System.out.print(set[i] + " ");
+		}
+		System.out.println();
+	}
+
+}
