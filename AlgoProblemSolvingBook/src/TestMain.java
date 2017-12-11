69bd8906a261888d7fe1ea41a30f7935c6f828aa
xuhu357 <xuhu357@gmail.com>
2017-12-11 16:48:32 +0900

    Make a class for testing some functions.

---
+++ AlgoProblemSolvingBook/src/TestMain.java
@@ -0,0 +1,13 @@
+
+public class TestMain {
+	
+	public static void main(String[] args) {
+		/**
+		 * This is the class for testing.
+		 */
+		System.out.println("This is the testing class.");
+		
+		
+	}
+
+}
