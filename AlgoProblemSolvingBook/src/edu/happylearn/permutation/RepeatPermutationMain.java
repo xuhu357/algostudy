33255c45b90f95f17fbc0f20b650044921a38180
xuhu357 <xuhu357@gmail.com>
2017-12-12 21:40:45 +0900

    n개의 숫자에서 k개를 중복가능한 순열 나열하기, Stack 버전

--- AlgoProblemSolvingBook/src/edu/happylearn/permutation/RepeatPermutationMain.java
+++ AlgoProblemSolvingBook/src/edu/happylearn/permutation/RepeatPermutationMain.java
@@ -1,9 +1,11 @@
 package edu.happylearn.permutation;
 
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
- * n개의 숫자에서 k개를 중복가능한 순열 나열하기, 배열 버전 
+ * n개의 숫자에서 k개를 중복가능한 순열 나열하기, Stack 버전
  * @author hu.xu
  *
  */
@@ -11,7 +13,7 @@ import java.util.Scanner;
 public class RepeatPermutationMain {
 	int n;
 	int k;
-	int arr[];
+	Stack<Integer> numStack;
 	
 	public static void main(String[] args) {
 		RepeatPermutationMain m = new RepeatPermutationMain();
@@ -22,26 +24,28 @@ public class RepeatPermutationMain {
 		Scanner scan = new Scanner(System.in);
 		n = scan.nextInt();
 		k = scan.nextInt();
-		arr = new int[n];
+		numStack = new Stack<>();
 		scan.close();
 		
-		repeatablePermutation(arr, n, k, 0);
+		repeatablePermutation(numStack, n, k, 0);
 	}
 
-	private void repeatablePermutation(int[] arr, int n, int k, int len) {
+	private void repeatablePermutation(Stack<Integer> numStack, int n, int k, int len) {
 		if(len == k){
-			printData(arr, len);
+			printData(numStack, len);
 			return;
 		}
 		for(int i=0; i<n; i++){
-			arr[len] = i;
-			repeatablePermutation(arr, n, k, len+1);
+			numStack.push(i);
+			repeatablePermutation(numStack, n, k, len+1);
+			numStack.pop();
 		}
 	}
 
-	private void printData(int[] arr, int len) {
-		for(int i=0; i<len; i++){
-			System.out.print(arr[i] + " ");
+	private void printData(Stack<Integer> numStack, int len) {
+		Iterator<Integer> iter = numStack.iterator();
+		while(iter.hasNext()){
+			System.out.print(iter.next()+" ");
 		}
 		System.out.println();
 	}
