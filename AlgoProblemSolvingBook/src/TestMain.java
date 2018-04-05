a43a6532701d71cc6278378c029d7eb31997a1a6
xuhu357 <xuhu357@gmail.com>
2018-04-05 22:14:42 +0900

    기본적인 hashmap의 사용법 테스트.

--- AlgoProblemSolvingBook/src/TestMain.java
+++ AlgoProblemSolvingBook/src/TestMain.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestMain {
 	
@@ -6,8 +8,22 @@ public class TestMain {
 		 * This is the class for testing.
 		 */
 		System.out.println("This is the testing class.");
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		
+		map.put("aaa", 1);
+		map.put("bbb", 2);
+		map.put("ccc", 3);
 		
+		for (Map.Entry<String, Integer> it : map.entrySet()) {
+			String key = it.getKey();
+			int value = it.getValue();
+			
+			System.out.println("Key/Value: "+ key + ", "+ value);
+		}
+		
+		for (int num : map.values()) {
+			System.out.println("value: "+ num);
+		}
 	}
 
 }
